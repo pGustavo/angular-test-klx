e20cc582af8314dd3f4d0cc603fd3bbd7f5b4f96
pGustavo <pGustavo@users.noreply.github.com>
2023-05-04 22:25:15 +0100

    1 file modified

--- app/services/data.ts
+++ app/services/data.ts
@@ -1,6 +1,16 @@
-export const data = {
-  img: 'https://cld.accentuate.io/5353320710301/1663094662150/Madrid_Heathered-Grey_Feature-Float-1380.png?v=1669223444554&options=w1000',
-  label: 'Amazing Shoe',
-  price: 100,
-  discount: 10,
-};
+export const data = [
+  {
+    id: 1,
+    img: 'https://cld.accentuate.io/5353320710301/1663094662150/Madrid_Heathered-Grey_Feature-Float-1380.png?v=1669223444554&options=w1000',
+    label: 'Amazing Shoe',
+    price: 100,
+    discount: 10,
+  },
+  {
+    id: 2,
+    img: 'https://cld.accentuate.io/5353320710301/1663094662150/Madrid_Heathered-Grey_Feature-Float-1380.png?v=1669223444554&options=w1000',
+    label: 'Amazing Shoe',
+    price: 100,
+    discount: 10,
+  },
+];
